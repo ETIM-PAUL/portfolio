bfb9ad56b004ce216a97832c63ecf75cc2c7a916
ETIM-PAUL <etimpaul22@gmail.com>
2022-06-22 00:08:42 +0100

    Updates

---
+++ precache-manifest.dca962d7c180dbe006732f0d82d254b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52aa36daf0ec3c5f236aa0881eb129e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3de2a3dfebf74af77121",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ae1dfad15e462640209e",
+    "url": "/portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "3de2a3dfebf74af77121",
+    "url": "/portfolio/static/js/2.5e4f7429.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.5e4f7429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1dfad15e462640209e",
+    "url": "/portfolio/static/js/main.c1b60f21.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
